257292ffaa4b681cb3f10e00c9f6068f68aa0785
mxlz070 <mxlz070@gmail.com>
2023-03-23 17:57:16 +0300

    hw 3-4

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from aiogram.utils import executor
 from config import dp
 from handlers import client, callback, extra, admin, FSM_Admin_mentors
 import logging
+from database.bot_db import sql_create
+async def on_startup(_)
+    sql_create()
+
 
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
@@ -13,4 +17,4 @@ extra.register_handlers_extra(dp)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    executor.start_polling(dp, skip_updates=True)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)
