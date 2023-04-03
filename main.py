903a337f660ce24dd7fff1acb8ae33b5293f024e
mxlz070 <mxlz070@gmail.com>
2023-04-03 18:02:16 +0300

    'hw7'

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ async def on_startup(_):
     sql_create()
     await sheduler.set_scheduler()
 
-
 client.register_handlers_client(dp)
 callback.register_handlers_callback(dp)
 FSM_Admin_mentors.register_handlers_fsm_anketa(dp)
+
 
 admin.register_handlers_admin(dp)
 
